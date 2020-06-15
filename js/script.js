ed17c451c63bbd1726743a7f7fc5288c4a0768ee
Hannington Odhiambo <36677724+Tony2308@users.noreply.github.com>
2020-06-15 15:58:44 +0300

    Fisrt commit

---
+++ js/script.js
@@ -0,0 +1,9 @@
+ function 	openSideBar(){
+ 	//create aside
+ 	alert('my aside bar!!!');
+ 	let aside = document.createElement('aside');
+ 	let text = document.createText('This is aside bar');
+
+ 	aside.appendChild(text);
+ 	
+ }
